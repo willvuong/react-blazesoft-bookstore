0edd5e9d6f1fd9f268c580da71797241974e39f0
William <wvuong3@myseneca.ca>
2021-09-22 13:08:32 -0400

    initial commit, added components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="Header">
+            <h2>Blazesoft Bookstore</h2>
+        </header>
+    )
+}
+
+export default Header
