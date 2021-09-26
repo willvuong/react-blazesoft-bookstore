bc46993c9ba8714a06ace7fa48b4dc160c150f08
William <wvuong3@myseneca.ca>
2021-09-26 16:42:31 -0400

    added Footer component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { useState } from 'react'
 import './App.css';
 import Header from './components/Header.js'
 import AddBook from './components/AddBook.js'
-import Books from './components/Books'
+import Books from './components/Books.js'
+import Footer from './components/Footer.js'
 
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
@@ -16,8 +17,8 @@ const App = () => {
     <div className="App">
       <Header />
       <AddBook />
-      <Books/>
-
+      <Books />
+      <Footer />
     </div>
     </Provider>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <a href="https://github.com/willvuong/react-blazesoft-bookstore">
+                <i class="fab fa-github"></i>
+            </a>
+            <a href="http://www.williamvuong.ca/">
+                <i class="fas fa-link"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Footer
