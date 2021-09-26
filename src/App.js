4f22c715e216fab615e55cbffe4f913ef957a803
William <wvuong3@myseneca.ca>
2021-09-26 16:26:05 -0400

    removed AddBookButton

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState } from 'react'
 
 import './App.css';
 import Header from './components/Header.js'
-import AddBookButton from './components/AddBookButton'
 import AddBook from './components/AddBook.js'
 import Books from './components/Books'
 
@@ -16,13 +15,9 @@ const App = () => {
     <Provider store={store}> 
     <div className="App">
       <Header />
-      {/* <AddBookButton
-        icon={showAddBook ? <i class="fas fa-times fa-2x"></i> : <i class="fas fa-plus fa-2x"></i>}
-        onClick={() => setShowAddBook(!showAddBook)}
-      />
-      {showAddBook && <AddBook/>} */}
       <AddBook />
       <Books/>
+
     </div>
     </Provider>
   );
